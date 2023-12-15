1a679795cfcf95dd59a2d7040de459ce5e9dc954
Nafees Bin Zaman <zamannafees@gmail.com>
2023-12-15 22:24:10 +0100

    Repo initialised

---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/b.png',
+            width: 200,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'Quiz Me',
+            style: TextStyle(
+                color: Color.fromARGB(255, 19, 90, 100),
+                fontSize: 28,
+                fontWeight: FontWeight.w700),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 241, 202, 27)),
+            label: const Text(
+              'Start Quiz',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w300),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+          )
+        ],
+      ),
+    );
+  }
+}
